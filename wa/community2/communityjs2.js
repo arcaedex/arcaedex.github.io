296de16caa8529776400fcc645b390c96bb4a9db
arcaedex <154298044+arcaedex@users.noreply.github.com>
2025-09-25 10:44:57 -0600

    setting up community2 wa6

---
+++ wa/community2/communityjs2.js
@@ -0,0 +1,25 @@
+const navToggle = document.querySelector('.nav-toggle');
+const hamburger = document.querySelectorAll('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+var menu_shown= false;
+
+function showMenu(){
+    var shown = navMenu.classList.toggle("show");
+    navMenu.classList.toggle("hide");
+
+    if (shown){
+        navToggle.setAttribute("aria-expanded", "true");
+        hamburger[0].style.backgroundColor='grey';
+        hamburger[1].style.backgroundColor='grey';
+        hamburger[2].style.backgroundColor='grey';
+        navToggle.setAttribute('aria-label', 'Close Navigation Menu');
+    } else {
+        navToggle.setAttribute("aria-expanded", "false");
+        hamburger[0].style.backgroundColor='black';
+        hamburger[1].style.backgroundColor='black';
+        hamburger[2].style.backgroundColor='black';
+        navToggle.setAttribute('aria-label', 'Open Navigation Menu');
+    }
+}
+navToggle.addEventListener('click', showMenu);
