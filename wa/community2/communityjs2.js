232af3d90fde46d0d12db32bcc116a41e80f5d65
arcaedex <154298044+arcaedex@users.noreply.github.com>
2025-09-28 15:54:43 -0600

    community 2 update

--- wa/community2/communityjs2.js
+++ wa/community2/communityjs2.js
@@ -1,3 +1,4 @@
+// this is for the ""hamburger"" menu
 const navToggle = document.querySelector('.nav-toggle');
 const hamburger = document.querySelectorAll('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
@@ -23,3 +24,38 @@ function showMenu(){
     }
 }
 navToggle.addEventListener('click', showMenu);
+
+//this is for the guestbook
+document.getElementById('guestnameButton').addEventListener("click", signBook);
+
+function signBook(){
+    let nameInput = document.getElementById('guestname').value;
+    if (nameInput === ""){
+        document.getElementById("book").innerHTML = "You didn't put anything in, silly!";
+    } else {
+document.getElementById("book").innerHTML = "Thanks for signing, " + nameInput + "!";
+    }
+}
+
+// section for resource buttons
+const filterButtons = document.querySelectorAll('.resourceButton button');
+const gridCards = document.querySelectorAll('.resource-card');
+
+// Add click event to each resourcebutton
+filterButtons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    const filterValue = event.target.textContent.toLowerCase();
+    filterGrid(filterValue);
+  });
+});
+
+function filterGrid(category) {
+    
+  gridCards.forEach(card => {
+    if (category === 'all' || card.dataset.category === category) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
