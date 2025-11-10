a6702125d138fe05b2a622f1844bab62e4587977
arcaedex <154298044+arcaedex@users.noreply.github.com>
2025-11-09 17:18:01 -0700

    wa12 implementation

--- wa/week12/wa12.js
+++ wa/week12/wa12.js
@@ -1,12 +1,17 @@
 //LOCALSTORAGE SETUP
+let timeList = [];
+if(localStorage.getItem("timeList")){
+  timeList = JSON.parse(localStorage.getItem("timeList"));
+}
 let myList = [];
 if(localStorage.getItem("savedList")){
   myList = JSON.parse(localStorage.getItem("savedList"));
   for (item in myList) {
-  htmlList(myList[item]);
+    //get index of item, use index to find equal in timeList
+  htmlList(myList[item], timeList[item]);
 }
 }
-//console.log(myList);
+//console.log(myList + " and time: " + timeList);
 
 //ON LOAD
 window.addEventListener('load', function() {
@@ -16,19 +21,20 @@ window.addEventListener('load', function() {
 //reset button
 let resetBtn = document.getElementById('clearbtn').addEventListener('click', resetData);
 function resetData(){
-  //console.log("clicked!");
+  //console.log("reset clicked!");
   localStorage.clear();
   for(i in myList){
     document.getElementById("movielist").removeChild(document.querySelector("li"));
   }
   myList = [];
+  timeList = [];
 }
 
 //export button
 let exportBtn = document.getElementById('exportbtn').addEventListener('click', exportData);
 function exportData(){
   //console.log("clicked 2!");
-  navigator.clipboard.writeText(JSON.stringify(localStorage));
+  navigator.clipboard.writeText(JSON.stringify(localStorage.savedList));
   alert('Copied to Clipboard!');
 }
 
@@ -39,6 +45,7 @@ const textInput = document.getElementById('searchbar');
       if (event.key === 'Enter') {
         //console.log('Enter key pressed!');
         movieSearch();
+        textInput.value = ""
       }
     });
 
@@ -68,24 +75,60 @@ async function findMovie(search) {
         }
         
         //console.log(json);
-        appendMovie(json.Title);
+        appendMovie(json.Title, json.Runtime);
 
     } catch (err) {
         console.log(err);
     }
 }
 // taking movie from search and pushing to myList, then calling htmlList to update html live
-function appendMovie(value){
+function appendMovie(value, time){
   myList.push(value);
-  //console.log(myList);
+  timeList.push(time);
   localStorage.setItem("savedList", JSON.stringify(myList));
-  htmlList(value);
+  localStorage.setItem("timeList", JSON.stringify(timeList));
+  htmlList(value, time);
 }
 
 // adding something to the html list
-function htmlList(text){
+function htmlList(text, time){
   let ul = document.getElementById("movielist");
-  let li = document.createElement("li")
+  let li = document.createElement("li");
+  let timeslot = document.createElement("li");
+  timeslot.textContent = time;
+  timeslot.setAttribute("class","runtime");
   li.textContent = text;
   ul.appendChild(li);
+  li.setAttribute("id",text);
+  let btn = document.createElement("button");
+  btn.textContent = "X";
+  btn.setAttribute("aria-label","Remove this movie");
+  li.appendChild(btn);
+  btn.setAttribute("class","removebtn");
+  addRemover(btn);
+  li.appendChild(timeslot);
+}
+
+//adding a remove button
+let removeBtns = document.querySelectorAll('.removebtn');
+//console.log(removeBtns);
+function addRemover(element){
+  let specificItem = element.parentNode.getAttribute("id");
+  element.addEventListener('click', function (){removeMovie(specificItem)});
+  //for the above^ adding the function bit like that prevents an auto-fire on load
+}
+
+//removeBtn.addEventListener('click', removeMovie);
+//remove list item function via attached removebtn
+function removeMovie(specific){
+  //console.log("you just tried to remove a movie: " + specific);
+  let element = document.getElementById(specific);
+  element.remove();
+  let myindex = myList.indexOf(specific);
+  if (myindex !== -1) {
+       myList.splice(myindex, 1);
+       timeList.splice(myindex, 1);
+    }
+  localStorage.setItem("savedList", JSON.stringify(myList));
+  localStorage.setItem("timeList", JSON.stringify(timeList));
 }
